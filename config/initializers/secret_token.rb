b4c4c790dfc02711e28e401a2cf3c07a3656842b
matsv339 <matsv339@student.liu.se>
2014-05-14 21:41:19 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Huckleberry::Application.config.secret_key_base = '706db1690d892500b260b9f70b19f58046298573dbc0f6ff2395f997afed5d21a60b0f123d287586f96f33d95819d04d7cbf00921987a74c0e462d0bfd4298f4'
